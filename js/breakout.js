13a8ba3e7337a3fa83d988ee9ee36dbdc9f96ccf
noelduffy444 <35809400+noelduffy444@users.noreply.github.com>
2018-02-22 16:33:12 +0000

    Starting the Game

--- js/breakout.js
+++ js/breakout.js
@@ -1,20 +1,18 @@
+//Setup the canvas
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 
-ctx.beginPath();
-ctx.rect(20, 40, 100, 50);
-ctx.fillStyle = "#FF0000";
-ctx.fill();
-ctx.closePath();
+//Set the starting point
+var x = canvas.width/2;
+var y = canvas.height-30;
 
-ctx.beginPath();
-ctx.arc(240, 160, 20, 0, Math.PI*2, false);
-ctx.fillStyle = "Green";
-ctx.fill();
-ctx.closePath();
+//Draw the ball
+function draw() {
+	ctx.beginPath();
+	ctx.arc(x, y, 10, 0, Math.PI*2);
+	ctx.fillStyle = "#0095DD";
+	ctx.fill();
+	ctx.closePath();
+}
 
-ctx.beginPath();
-ctx.rect(430, 270, 40, 40);
-ctx.fillStyle = "#FFFF00";
-ctx.fill();
-ctx.closePath();
+setInterval(draw, 10);

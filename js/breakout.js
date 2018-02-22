82756d18b7f55b9d5ff7322cb70fc4870aa77f9e
noelduffy444 <35809400+noelduffy444@users.noreply.github.com>
2018-02-22 16:54:46 +0000

    Move the ball

--- js/breakout.js
+++ js/breakout.js
@@ -4,15 +4,24 @@ var ctx = canvas.getContext("2d");
 
 //Set the starting point
 var x = canvas.width/2;
+var dx = 2;
 var y = canvas.height-30;
+var dy = -2;
 
 //Draw the ball
-function draw() {
+function drawBall() {
 	ctx.beginPath();
-	ctx.arc(x, y, 10, 0, Math.PI*2);
+	ctx.arc(x, y, ballRadius, 0, Math.PI*2);
 	ctx.fillStyle = "#0095DD";
 	ctx.fill();
 	ctx.closePath();
+}
+
+function draw() {
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+	drawball();
+	x += dx;
+	y += dy;
 }
 
 setInterval(draw, 10);

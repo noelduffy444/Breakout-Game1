eda69f99cc09e7a54fcb120a14f0d288087f2fc8
noelduffy444 <35809400+noelduffy444@users.noreply.github.com>
2018-03-20 22:32:18 +0000

    mouse

--- Breakout-Game1-master/js/breakout.js
+++ Breakout-Game1-master/js/breakout.js
@@ -29,6 +29,7 @@ for(c=0; c<brickColumnCount; c++) {
 
 document.addEventListener("keydown", keyDownHandler, false);
 document.addEventListener("keyup", keyUpHandler, false);
+document.addEventListener("mousemove", mouseMoveHandler, false);
 
 function keyDownHandler(e) {
     if(e.keyCode == 39) {
@@ -44,6 +45,12 @@ function keyUpHandler(e) {
     }
     else if(e.keyCode == 37) {
         leftPressed = false;
+    }
+}
+function mouseMoveHandler(e) {
+    var relativeX = e.clientX - canvas.offsetLeft;
+    if(relativeX > 0 && relativeX < canvas.width) {
+        paddleX = relativeX - paddleWidth/2;
     }
 }
 function collisionDetection() {
